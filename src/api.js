6f5d478b2a703f2a1df03b6a97dc9046c51259e6
Retch <retch100@gmail.com>
2021-11-06 17:22:46 +0100

    update for netlify

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 const express = require('express');
-const { json } = require('express');
 const app = express();
+const router = express.Router();
 
 const uri = "mongodb+srv://netlify:W8pWilx29fqTrSQt@cluster0.q42oh.mongodb.net";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -10,15 +10,13 @@ client.connect();
 const hauptspeise = client.db("Rezepte").collection("Hauptspeise");
 
 
-
-
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
   res.send({
     message: 'Welcome to the API'
   });
 });
 
-app.get('/api/hauptspeise/random/:amount', (req, res) => {
+app.get('/hauptspeise/random/:amount', (req, res) => {
   const amount = parseInt(req.params.amount);
   hauptspeise.aggregate([{ $sample: { size: amount } }]).toArray((err, result) => {
     if (err) {
@@ -29,7 +27,7 @@ app.get('/api/hauptspeise/random/:amount', (req, res) => {
   });
 });
 
-app.get('/api/hauptspeise/random', (req, res) => {
+app.get('/hauptspeise/random', (req, res) => {
   hauptspeise.aggregate([{ $sample: { size: 1 } }]).toArray((err, result) => {
     if (err) {
       res.send(err);
@@ -39,7 +37,7 @@ app.get('/api/hauptspeise/random', (req, res) => {
   });
 });
 
-app.get('/api/rezept/id/:recipeid', (req, res) => {
+app.get('/rezept/id/:recipeid', (req, res) => {
   const id = parseInt(req.params.recipeid);
   hauptspeise.find({"_id": id}).toArray((err, result) => {
     if (err) {
@@ -51,5 +49,5 @@ app.get('/api/rezept/id/:recipeid', (req, res) => {
 });
 
 
-
-app.listen(8080);
+app.use('/.netlify/functions/api', router)
+module.exports.handler = serverless(app);
